28cb5cfee367e82221e7cbafe983703095cf2756
aim-ctrl <adlouni.m@gmail.com>
2025-12-19 14:35:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown("""
     footer { visibility: hidden; }
     
     .block-container {
-        padding: 0.5rem 5rem !important;
+        padding: 1.5rem 1rem !important;
         max-width: 100%;
     }
 
