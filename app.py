5cecbb50d9dd84151909207077942a865dba828a
aim-ctrl <adlouni.m@gmail.com>
2025-12-19 15:10:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown("""
 
     /* 2. HEADER (Uppdaterad höjd) */
     .top-bar {
-        flex: 0 0 5px; /* <-- Minskad höjd här (var 60px) */
+        flex: 0 0 0 px; /* <-- Minskad höjd här (var 60px) */
         display: flex;
         align-items: center;
         justify-content: space-between;
