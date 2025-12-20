09db60e2c3cb3819ef6372a0268b5da91edf2bee
aim-ctrl <adlouni.m@gmail.com>
2025-12-21 00:55:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ st.markdown("""
         text-align: center;
         color: #000;
         width: 100%;
-        padding: 250px 10px;
+        padding: 0px 10px;
     }
     
     .header-wrapper {
